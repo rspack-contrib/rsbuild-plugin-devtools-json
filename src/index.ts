24f622504038705505e1200a53887705fa58b4e3
neverland <chenjiahan.jait@bytedance.com>
2025-06-22 20:50:36 +0800

    refactor: setup middleware in onBeforeStartDevServer

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,6 @@ export const pluginDevtoolsJson = (
 ): RsbuildPlugin => ({
   name: 'rsbuild-plugin-devtools-json',
   setup(api) {
-    if (process.env.NODE_ENV !== 'development') {
-      return;
-    }
-
     const getOrCreateUUID = () => {
       if (options?.uuid) {
         return options.uuid;
@@ -56,44 +52,31 @@ export const pluginDevtoolsJson = (
       return uuid;
     };
 
-    api.modifyRsbuildConfig((userConfig, { mergeRsbuildConfig }) => {
-      return mergeRsbuildConfig(userConfig, {
-        dev: {
-          setupMiddlewares: [
-            (middlewares) => {
-              middlewares.push((req, res, next) => {
-                if (req.url !== ENDPOINT) {
-                  return next();
-                }
+    api.onBeforeStartDevServer(({ server }) => {
+      server.middlewares.use((req, res, next) => {
+        if (req.url !== ENDPOINT) {
+          return next();
+        }
 
-                let root = api.context.rootPath;
+        let root = api.context.rootPath;
 
-                // WSL case detection
-                if (process.env.WSL_DISTRO_NAME) {
-                  // Convert Linux path to Windows path format for WSL
-                  root = path
-                    .join(
-                      '\\\\wsl.localhost',
-                      process.env.WSL_DISTRO_NAME,
-                      root,
-                    )
-                    .replace(/\//g, '\\');
-                }
+        // WSL case detection
+        if (process.env.WSL_DISTRO_NAME) {
+          // Convert Linux path to Windows path format for WSL
+          root = path
+            .join('\\\\wsl.localhost', process.env.WSL_DISTRO_NAME, root)
+            .replace(/\//g, '\\');
+        }
 
-                const uuid = getOrCreateUUID();
-                const devtoolsJson: DevToolsJSON = {
-                  workspace: {
-                    root,
-                    uuid,
-                  },
-                };
-                res.setHeader('Content-Type', 'application/json');
-                res.end(JSON.stringify(devtoolsJson, null, 2));
-              });
-              return middlewares;
-            },
-          ],
-        },
+        const uuid = getOrCreateUUID();
+        const devtoolsJson: DevToolsJSON = {
+          workspace: {
+            root,
+            uuid,
+          },
+        };
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(devtoolsJson, null, 2));
       });
     });
   },
